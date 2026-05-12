f1bbbc88334a8c4ea6e71106b374a6fe5804c69a
Nat-200606 <nataliamaciel20006@gmail.com>
2026-05-12 19:20:17 -0300

    Implemented txt files to save data

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static int conta = 0;
     public static int saldo = 1;
     public static int pos = 1;
-    public static int posAtual;
     public static void main(String[] args) {
 
         FrameInicial inicial = new FrameInicial();
